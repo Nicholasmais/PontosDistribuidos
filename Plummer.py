1ab18c09a1df53492ba2a93aa39049a1fdad4f2e
Nícholas Eras <48137930+Nicholamais@users.noreply.github.com>
2021-02-24 22:22:13 -0300

    Esfera done

--- Plummer.py
+++ Plummer.py
@@ -1,63 +1,49 @@
 import plotly.express as px
-import random
-import numpy as np
+from numpy import  sin, cos, pi, random, arccos, sqrt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 a = 3*3.086*10**(19)
 
-x = y = z = raio = []
-ponto = [[0]*100000,[0]*100000,[0]*100000,[0]*100000]
-pontos = [[],[],[],[]
-]
-for i in range (0,100000):
-    r = a / np.sqrt(np.random.uniform(0, 1) ** (-2.0 / 3.0) - 1)
-    phi = (random.uniform(0,2*np.pi))
-    the = np.arccos(np.random.uniform(-1,1))
+phi = random.uniform(0, 2 * pi,10000)
+theta = arccos(random.uniform(-1,1, 10000))
+r = a / (sqrt((random.uniform(0, 1,10000)**(-2.0 / 3.0) - 1)))
 
-    x = r*np.cos(phi)*np.sin(the)
-    y = r*np.sin(phi)*np.sin(the)
-    z = r*np.cos(the)
-
-
-    ponto[0][i] = x
-    ponto[1][i] = y
-    ponto[2][i]= z
-
-    raio.append(r)
-
-
-    ponto[3][i] = raio[i]
-
-
-media = sum(ponto[3])/100000
-for i in range(0,100000):
-    if ponto[3][i] < 50*media:
-        pontos[0].append(ponto[0][i])
-        pontos[1].append(ponto[1][i])
-        pontos[2].append(ponto[2][i])
-        pontos[3].append(ponto[3][i])
+x = ( r * sin(theta) * cos(phi))
+y = ( r * sin(theta) * sin(phi))
+z = (r * cos(theta))
 
 df = px.data.gapminder()
-fig = px.scatter_3d(df, x = pontos[0], y = pontos[2], z=pontos[1], color=pontos[3])
+fig = px.scatter_3d(df, x = x, y = y, z=z)
 
-for template in ["plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "simple_white", "none"]:
-    fig.update_layout(template='plotly_dark')
-
-
+fig.update_layout(template='plotly_dark')
 fig.update_traces(marker=dict(size=1))
 
-#fig.show()
+fig.show()
+
+plano = make_subplots(specs=[[{"secondary_y": True}]])
+
+plano.update_layout(xaxis_title="Porcentagem do raio", yaxis_title="Quantidade de pontos")
+
+yy = [0]*10
+eixox = ['0-10','10-20','20-30','30-40','40-50','50-60','60-70','70-80','80-90','90-100']
+R = max(r)
+
+for ra in range(0, 10):
+     temp = []
+     for k in range(0, 10000):
+          if ra/100 * R + .1*R >= r[k] > ra/100 * R:
+               temp.append(x[k])
+
+     yy[ra] = len(temp)
 
 
-numbers = [0, 0, 0, 0]
-volume = [0,0,0,0]
-densi= [0,0,0,0]
-eixox = ['10^18', '10^19', '10^20', '10^21']
+func = [0,0,0,0,0,0,0,0,0,0]
 
-for i in range(0, 4):
-    numbers[i] = (sum(j <= 10**(18+i) for j in ponto[3]))
-    volume[i] = 4 / 3 * np.pi * (10**(18+i)) ** 3
-    densi[i] = numbers[i] / volume[i] * 10**56
+for i in range(0,10):
+     func[i] = (i/a)**3 / ((1+(r/a)**2 )**3/2) * 10**5
 
-plano = px.scatter(x=eixox, y=densi)
-plano.update_layout(xaxis_title="Raio", yaxis_title="Densidade de pontos (10^-3)",yaxis_range=[0,5])
+plano.add_trace(go.Scatter(x=eixox, y=yy, name="Pontos"),secondary_y=False)
+plano.add_trace(go.Scatter(x=eixox, y=func, name="Função"),secondary_y=False,)
+
 plano.show()
--- esfera.py
+++ esfera.py
@@ -1,37 +1,42 @@
 import plotly.express as px
-from numpy import  sin, cos, pi, random, array, arccos
+from numpy import sin, cos, pi, random, arccos
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-phi = random.uniform(0, 2 * pi,5000)
-costheta = random.uniform(-1,1,5000)
+phi = random.uniform(0, 2 * pi, 10000)
+costheta = random.uniform(-1, 1, 10000)
 theta = arccos(costheta)
-u = random.uniform(0, 1,5000)
+u = random.uniform(0, 1, 10000)
 
-r = ( 100 * (u) ** (1 / 3))
-x = ( r * sin(theta) * cos(phi))
-y = ( r * sin(theta) * sin(phi))
+R = 100
+r = (R * (u) ** (1 / 3))
+x = (r * sin(theta) * cos(phi))
+y = (r * sin(theta) * sin(phi))
 z = (r * cos(theta))
 
-yy = [0]*10
-eixox = ['0-10','10-20','20-30','30-40','40-50','50-60','60-70','70-80','80-90','90-100']
+raio = [10 * i for i in range(11)]
+mr = [10000 * (raio[i] / R) ** 3 for i in range(0, 11)]
 
+yy = [0] * 11
+eixox = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
 
-for ra in range(0, 10):
-     temp = []
-     for k in range(0, 5000):
-          if 10 * ra + 10 >= r[k] > 10 * ra:
-               temp.append(x[k])
-     yy[ra] = len(temp)
+for ra in range(0, 11):
+    temp = []
+    for k in range(0, 10000):
+        if 10 * ra >= r[k]:
+            temp.append(x[k])
+    yy[ra] = len(temp)
 
 df = px.data.iris()
 
-fig = px.scatter_3d(df, x = x, y = y, z=z, title='Distribuição de pontos em um esfera')
+fig = px.scatter_3d(df, x=x, y=y, z=z, title='Distribuição de pontos em um esfera')
 fig.update_traces(marker=dict(size=1))
-fig.update_layout(template = 'plotly_dark',scene=dict(xaxis_title="Abscissa", yaxis_title="Ordenada", zaxis_title="Cota"))
+fig.update_layout(template='plotly_dark',
+                  scene=dict(xaxis_title="Abscissa", yaxis_title="Ordenada", zaxis_title="Cota"))
 fig.show()
 
-
-plano = px.line(x=eixox, y=yy, title="Esfera")
-plano.update_layout(xaxis_title="Raio", yaxis_title="Quantidade de pontos", yaxis_range = [0,3000])
+plano = make_subplots(specs=[[{"secondary_y": True}]])
+plano.update_layout(title='Esfera', xaxis_title="Raio", yaxis_title="Quantidade de pontos")
+plano.add_trace(go.Scatter(x=eixox, y=yy, name="Pontos"), secondary_y=False)
+plano.add_trace(go.Scatter(x=eixox, y=mr, name="Função"), secondary_y=False, )
 plano.show()
-
-print(sum(yy))
